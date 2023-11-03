561f61411748f736a04ef2b41fd766f0c9f86127
acousticsdk <Acousticpwnz1>
2023-11-03 16:15:34 +0200

    adding debounce

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-Object.prototype.forEach = function (callback) { return Object.keys(this).map(key => callback(key, this[key])) }; 
+// add forEach to object prototype
+Object.prototype.forEach = function (callback) { return Object.keys(this).map(key => callback(key, this[key])) };
+// -------------------------------
+
+// simple debounce
+function debounce(func){
+    let timer;
+    return function(event){
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(func,300,event);
+    };
+}
+window.addEventListener("resize",debounce(function(e){
+   alert('resize finished')
+}));
+// -------------------------------
